87e113cd4fc5af2a639a6874c9aaad84840bc1e5
n4n5 <its.just.n4n5@gmail.com>
2024-03-13 17:39:24 +0100

    add

--- src/panels/socket_manager.rs
+++ src/panels/socket_manager.rs
@@ -6,7 +6,7 @@ use std::rc::Rc;
 pub struct SocketManager {
     ws_sender: Rc<RefCell<WsSender>>,
     text_to_send: String,
-    msg_id: u32,
+    msg_id: u64,
 }
 
 impl SocketManager {
@@ -39,6 +39,91 @@ impl super::PanelController for SocketManager {
     }
 }
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+struct Layers {
+    #[serde(rename(serialize = "PHY"))]
+    phy: String,
+    #[serde(rename(serialize = "MAC"))]
+    mac: String,
+    #[serde(rename(serialize = "RLC"))]
+    rlc: String,
+    #[serde(rename(serialize = "PDCP"))]
+    pdcp: String,
+    #[serde(rename(serialize = "RRC"))]
+    rrc: String,
+    #[serde(rename(serialize = "NAS"))]
+    nas: String,
+    #[serde(rename(serialize = "S72"))]
+    s72: String,
+    #[serde(rename(serialize = "S1AP"))]
+    s1ap: String,
+    #[serde(rename(serialize = "NGAP"))]
+    ngap: String,
+    #[serde(rename(serialize = "GTPU"))]
+    gtpu: String,
+    #[serde(rename(serialize = "X2AP"))]
+    x2ap: String,
+    #[serde(rename(serialize = "XnAP"))]
+    xnap: String,
+    #[serde(rename(serialize = "M2AP"))]
+    m2ap: String,
+    #[serde(rename(serialize = "LPPa"))]
+    lppa: String,
+    #[serde(rename(serialize = "NRPPa"))]
+    nrppa: String,
+    #[serde(rename(serialize = "TRX"))]
+    trx: String,
+}
+impl Layers {
+    pub fn new() -> Self {
+        Self {
+            phy: "warn".to_owned(),
+            mac: "warn".to_owned(),
+            rlc: "warn".to_owned(),
+            pdcp: "warn".to_owned(),
+            rrc: "debug".to_owned(),
+            nas: "debug".to_owned(),
+            s72: "warn".to_owned(),
+            s1ap: "warn".to_owned(),
+            ngap: "warn".to_owned(),
+            gtpu: "warn".to_owned(),
+            x2ap: "warn".to_owned(),
+            xnap: "warn".to_owned(),
+            m2ap: "warn".to_owned(),
+            lppa: "warn".to_owned(),
+            nrppa: "warn".to_owned(),
+            trx: "warn".to_owned(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct LogGet {
+    timeout: u64,
+    min: u64,
+    max: u64,
+    layers: Layers,
+    message: String,
+    headers: bool,
+    message_id: u64,
+}
+
+impl LogGet {
+    pub fn new(id: u64) -> Self {
+        Self {
+            timeout: 1,
+            min: 64,
+            max: 2048,
+            layers: Layers::new(),
+            message: "log_get".to_owned(),
+            headers: false,
+            message_id: id,
+        }
+    }
+}
+
 impl super::PanelView for SocketManager {
     fn ui(&mut self, ui: &mut egui::Ui) {
         ui.horizontal(|ui| {
@@ -47,11 +132,14 @@ impl super::PanelView for SocketManager {
             }
             if ui.button("Next").clicked() {
                 log::info!("Next");
-                // TODO message_id
-                let msg = r#"
-                            {"timeout":1,"min":64,"max":2048,"layers":{"PHY":"warn","MAC":"warn","RLC":"warn","PDCP":"warn","RRC":"debug","NAS":"debug","S72":"warn","S1AP":"warn","NGAP":"warn","GTPU":"warn","X2AP":"warn","XnAP":"warn","M2AP":"warn","LPPa":"warn","NRPPa":"warn","TRX":"warn"},"message":"log_get","headers":false,"message_id":1}
-                            "#;
-                self.ws_sender.borrow_mut().send(WsMessage::Text(msg.into()));
+                let msg = LogGet::new(self.msg_id);
+                self.msg_id += 1;
+                if let Ok(msg_stringed) = serde_json::to_string(&msg) {
+                    log::info!("{}", msg_stringed);
+                    self.ws_sender
+                        .borrow_mut()
+                        .send(WsMessage::Text(msg_stringed));
+                }
             }
             ui.separator();
             ui.horizontal(|ui| {
